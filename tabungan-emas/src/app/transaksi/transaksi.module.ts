27966186f277a98d60c91b7ea0180b0cba8ec799
ciazhar <daisetsunakama@gmail.com>
2017-05-20 06:41:22 +0700

    routing transaksi

--- tabungan-emas/src/app/transaksi/transaksi.module.ts
+++ tabungan-emas/src/app/transaksi/transaksi.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { JualComponent } from './jual/jual.component';
 import { BeliComponent } from './beli/beli.component';
 import { RekapComponent } from './rekap/rekap.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routingTransaksi : Routes = [
+  { path: "transaksi/beli", component: BeliComponent },
+  { path: "transaksi/jual", component: JualComponent },
+  { path: "transaksi/rekap", component: RekapComponent }
+]
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routingTransaksi)
   ],
   declarations: [JualComponent, BeliComponent, RekapComponent]
 })
